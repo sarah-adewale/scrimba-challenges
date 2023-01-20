3fda96ba60ec68cfacdff6d57f5149ed4cccb7e7
Sarah Adewale <saraphynaadewale@gmail.com>
2023-01-21 00:54:10 +0100

    added greatest product algorithm

---
+++ js/greatest-product.js
@@ -0,0 +1,13 @@
+// Given an array of positive numbers, write a function that returns the greatest product of any three numbers. 
+// The approach of using three nested loops would clock in at O(N3), which is very slow. Use sorting to implement the 
+// function in a way that it computes at O(N log N) speed. (There are even faster implementations, but we’re focusing on using
+
+function greatestProduct(arr){
+    let sortedArr = arr.sort((a,b) => a > b ? -1 : 1)
+
+    for(let i = 0; i < sortedArr.length - 1; i++){
+        return arr[arr.length - 1] * arr[arr.length - 2] * arr[arr.length - 2]
+    }
+}
+
+console.log(greatestProduct([3,2,4,7,8]))
