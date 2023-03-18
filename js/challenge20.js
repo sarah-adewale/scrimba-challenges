4e68785f30797ec6f821abb58304ccfc1d5c8695
Sarah Adewale <saraphynaadewale@gmail.com>
2023-03-18 14:15:46 +0100

    added challenge20 challenge

---
+++ js/challenge20.js
@@ -0,0 +1,53 @@
+const shoppingCart = [
+    {
+        item: "🍭",
+        price: 2.99,
+        type: "sweet",
+        onSale: true
+    },
+    {
+        item: "🍫",
+        price: 1.99, 
+        type: "sweet",
+        onSale: true
+    },
+    {
+        item: "🥫",
+        price: 1.99, 
+        type: "savory",
+        onSale: false
+    },
+    {
+        item: "🍬",
+        price: .89, 
+        type: "sweet",
+        onSale: false
+    },
+    {
+        item: "🥦",
+        price: 3.99,
+        type: "savory",
+        onSale: false
+    }, 
+    {
+        item: "🍖",
+        price: 3.99, 
+        type: "savory",
+        onSale: true
+    }, 
+]
+
+/*  
+Use reduce() to total the groceries. 
+Then find a method that will round the total to 2 decimal places.
+
+Example output: 73.44
+*/
+
+function total(arr){
+
+   return arr.reduce((acc, c) => acc + c.price, 0).toFixed(2)
+    
+}
+
+console.log(total(shoppingCart));
