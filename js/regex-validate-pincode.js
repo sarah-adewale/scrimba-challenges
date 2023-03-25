abf48bdaa17fc842c7c704a93e1c7010d4b7fa75
Sarah Adewale <saraphynaadewale@gmail.com>
2023-03-25 15:27:26 +0100

    added regex

--- js/regex-validate-pincode.js
+++ js/regex-validate-pincode.js
@@ -8,10 +8,31 @@
 // "a234"   -->  false
 
 
-function validatePIN (pin) {
-  //return true or false
-   return pin.length === 4 || pin.length === 6 
+function validatePIN(pin) {
+  const pinLength = pin.length;
+  
+  // Check if the PIN length is either 4 or 6
+  if (pinLength === 4 || pinLength === 6) {
+    // Check if all characters in the PIN are digits
+    for (let i = 0; i < pinLength; i++) {
+      if (!isDigit(pin.charAt(i))) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  return false;
 }
+
+function isDigit(char) {
+  return char >= "0" && char <= "9";
+}
+
+console.log(validatePIN("1234")); // true
+console.log(validatePIN("12345")); // false
+console.log(validatePIN("a234")); // false
+
 
 console.log(validatePIN('12345'), false)
 console.log(validatePIN('1234'), true)
