aacae914786eacaf44eb46d7371b67c9a9af5a9c
Sarah Adewale <saraphynaadewale@gmail.com>
2023-04-18 22:15:28 +0100

    finished challenge seven

--- js/challenge7.js
+++ js/challenge7.js
@@ -16,7 +16,8 @@ Example output: false
 */ 
 
 function isAnagram(str1, str2){
-return  str1.split('').sort().join('') === str2.split('').sort().join('')
+    if(str1.length !== str2.length) return false
+    return  str1.split('').sort().join('') === str2.split('').sort().join('')
 }
 
 console.log(isAnagram("allergy", "gallery"));
