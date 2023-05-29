4afdd597cc1fcd996637f7889b37ae7743e45d09
Sarah Adewale <saraphynaadewale@gmail.com>
2023-05-29 13:58:57 +0100

    modified longest word

--- js/longest-word-string.js
+++ js/longest-word-string.js
@@ -1,18 +1,30 @@
 //Given a sentence, return the longest word in the string eg
 // longestWord('Top Shelf Web Development Training on Stoch')
 
-function longestWord(str){
-    str = str.split(' ')
-    console.log(str)
-    let result = ''
-    let maxLength = 0
+// function longestWord(str){
+//     str = str.split(' ')
+//     console.log(str)
+//     let result = ''
+//     let maxLength = 0
 
-    for(let i = 0; i < str.length; i++){
-        if(str[i].length > maxLength){
-            maxLength = str[i].length
-            result = str[i]
+//     for(let i = 0; i < str.length; i++){
+//         if(str[i].length > maxLength){
+//             maxLength = str[i].length
+//             result = str[i]
+//         }
+//     }
+//     return result
+// }
+
+function longestWord(str){
+    let newStr = str.split(' ')
+    let result = newStr.reduce((maxLengthWord, currentWord) => {
+        if(currentWord.length > maxLengthWord.length){
+            return currentWord
+        }else{
+            return maxLengthWord
         }
-    }
+    }, '')
     return result
 }
 
