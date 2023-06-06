50989f73a88a1fcf0f7fdb037c0e898a1c594864
Sarah Adewale <saraphynaadewale@gmail.com>
2023-06-06 10:59:21 +0100

    added recursive algorithm

---
+++ js/recursively-recurse.js
@@ -0,0 +1,42 @@
+//the following function prints every number from a low number to a high number. for example, if low is 0 and high is 10
+
+// function printEveryOther(high, low){
+//     //base case
+//     if(low > high){
+//         return low
+//     }
+
+//     return printEveryOther(low += 2, high)
+// }
+
+// console.log(printEveryOther(10, 0))
+
+// here is an array containing both numbers as well as other arrays, which in turn contain numbers and arrays:
+
+// array = [ 1, 2,
+// 3,
+// [4, 5, 6], 7,
+// [8,
+// [9, 10, 11, [12, 13, 14]
+// Write a recursive function that prints all the numbers (and just numbers).
+
+function printItems(arr){
+    arr.forEach(element => {
+        if(Array.isArray(element)){
+            printItems(element)
+        }else{
+            return element
+        }
+    });
+}
+
+console.log(printItems([ 1, 2,
+3,
+[4, 5, 6], 7,
+[8,
+[9, 10, 11, [12, 13, 14]
+] ],
+[15, 16, 17, 18, 19, [20, 21, 22,
+[23, 24, 25, [26, 27, 29]
+], 30, 31 ], 32
+], 33 ]))
