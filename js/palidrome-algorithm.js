3bd5876fe7aabc5e5a8d9e372b0c1bb1799ea1e7
Sarah Adewale <saraphynaadewale@gmail.com>
2023-06-10 21:44:03 +0100

    palindrome checker algorithm

---
+++ js/palidrome-algorithm.js
@@ -0,0 +1,18 @@
+
+function palindromeChecker(str){
+    let leftIndex = 0
+    let rightIndex = str.length-1
+
+    for(let i = 0; i < str.length/2; i++){
+        if(str[leftIndex] !== str[rightIndex]){
+            return false
+        }else{
+            return true
+        }
+    }
+   
+}
+
+console.log(palindromeChecker('kayak'))
+console.log(palindromeChecker('madam'))
+console.log(palindromeChecker('kene'))
