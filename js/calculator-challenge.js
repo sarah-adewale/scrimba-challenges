3f2638dd7c8eceb3eb993ec2eabcc1f6baa8140d
Sarah Adewale <saraphynaadewale@gmail.com>
2023-07-13 13:41:20 +0100

    added calculator challenge

---
+++ js/calculator-challenge.js
@@ -0,0 +1,19 @@
+// create a function
+function calculator(num1, num2, operator){
+    if(operator === '+'){
+        console.log(num1 + num2)
+    }else if(operator === '-'){
+        console.log(num1 - num2)
+    }else if(operator === '*'){
+        console.log(num1 * num2)
+    }else if(operator === '/'){
+        console.log(num1 / num2)
+    }else{
+        console.log('invalid operator')
+    }
+}
+// create a conditional
+    // if operator is === '+'
+    //return num + num
+
+    calculator(4,5, '&')
