5246647119b821f4f58231586b2242345ec1f309
Sarah Adewale <saraphynaadewale@gmail.com>
2023-09-23 11:54:49 +0100

    added remove exclamation from sentence

---
+++ js/remove-exclamation-from-sentence.js
@@ -0,0 +1,23 @@
+// Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner 
+// kata, you can assume that the input data is always a non empty string, no need to verify it.
+
+// Examples
+// "Hi!"     ---> "Hi!"
+// "Hi!!!"   ---> "Hi!"
+// "!Hi"     ---> "Hi!"
+// "!Hi!"    ---> "Hi!"
+// "Hi! Hi!" ---> "Hi Hi!"
+// "Hi"      ---> "Hi!"
+
+function remove(string) {
+  //coding and coding....
+  string = string.split('!').join('')
+  return string + '!';  
+}
+
+
+console.log(remove("Hi!"), "Hi!")
+console.log(remove("Hi!!!"), "Hi!")
+console.log(remove("!Hi"), "Hi!")
+console.log(remove("Hi! Hi!"), "Hi Hi!")
+console.log(remove("Hi"), "Hi!")
